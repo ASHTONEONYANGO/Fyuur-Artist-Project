7ef3f04d7892a7c65759232f71c470e48305d3d7
ASHTONEONYANGO <onyangoashtone@gmail.com>
2022-08-25 23:17:42 +0300

    Commiting project changes

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = '<postgresql://postgres:hugo@localhost:5432/fyyurproject>'
